31d877a32ed734e752ae7a8f964084b27ab8dc4a
chaeyeon <chaeyeon5322@gmail.com>
2026-04-08 14:25:04 +0900

    feat: 하단 네비게이션 공통 컴포넌트 구현(#4)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
-<script setup>
-import { ref } from 'vue'
-import { RouterView } from 'vue-router'
-</script>
-
 <template>
-  <div>
-    <RouterView />
+  <!-- 전체 배경 -->
+  <div class="min-h-screen bg-gray-100 flex justify-center items-center">
+    <!-- 아이폰 틀 (390 x 844) -->
+    <div class="relative w-[390px] h-[844px] bg-white shadow-2xl overflow-hidden flex flex-col">
+      <!-- 상단 헤더 (NavBar.vue를 헤더로 사용) -->
+      <NavBar />
+
+      <!-- 메인 컨텐츠 (스크롤 영역) 
+      <main class="flex-1 overflow-y-auto bg-white">
+        <router-view />
+      </main>
+      -->
+      <!-- 하단 바 (BottomBar.vue를 하단 바로 사용) -->
+      <BottomBar />
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<script setup>
+import './assets/main.css'
+import NavBar from './components/common/NavBar.vue'
+import BottomBar from './components/common/BottomBar.vue'
+</script>
