c8ec99457f58d70baa33171af03635fe1892c059
YSH <yang5864@naver.com>
2026-04-09 09:32:42 +0900

    fix: NavBar bg-kb-yellow 로 변경

--- src/components/common/NavBar.vue
+++ src/components/common/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-[390px] h-[60px] bg-yellow-500 flex items-center justify-center px-4 relative">
+  <header class="w-[390px] h-[60px] bg-kb-yellow flex items-center justify-center px-4 relative">
     <div class="flex items-center justify-center">
       <img :src="logoUrl" alt="야금 로고" class="w-[130px] h-[80px] object-contain" />
     </div>
